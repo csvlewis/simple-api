5534fbac27ff3cea91d7bb4f32cea014dbecabc6
Christopher Lewis <13chlewis@gmail.com>
2025-05-13 15:15:13 -0600

    Implemented pg and kysely to connect to local postgres instance

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,36 @@
+import { Generated, Kysely, PostgresDialect, sql } from "kysely";
+import { Pool } from "pg";
+
 type Item = {
-  id: string;
+  id: Generated<string>;
   name: string;
   quantity: number;
 };
 
-export const items: Item[] = [];
+type Database = {
+  items: Item;
+};
+
+export const db = new Kysely<Database>({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      database: "test",
+      host: "localhost",
+      user: "admin",
+      port: 5432,
+      max: 10,
+    }),
+  }),
+});
+
+export async function initDb() {
+  await db.schema
+    .createTable("items")
+    .ifNotExists()
+    .addColumn("id", "uuid", (col) =>
+      col.primaryKey().defaultTo(sql`gen_random_uuid()`)
+    )
+    .addColumn("name", "text", (col) => col.notNull().unique())
+    .addColumn("quantity", "integer", (col) => col.notNull())
+    .execute();
+}
