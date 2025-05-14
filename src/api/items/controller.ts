aa1c57a3cfb131b5482d824fc4f4b8dce57cda19
Christopher Lewis <13chlewis@gmail.com>
2025-05-14 16:10:38 -0600

    Update import statement

--- src/api/items/controller.ts
+++ src/api/items/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import * as ItemService from "./service";
+import { getItems } from "./service";
 
 export function getAllItems(db: any) {
   return async (_req: Request, res: Response) => {
-    const items = await ItemService.getItems(db);
+    const items = await getItems(db);
     res.json(items);
   };
 }
