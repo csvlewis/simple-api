8c125910afaf0fca1a56d101fd969cd4cafa0535
Christopher Lewis <13chlewis@gmail.com>
2025-05-14 10:58:37 -0600

    Initialize DB on startup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import pingRouter from "./routes/ping";
+import { initDb } from "./lib/db";
 
 const app = express();
 app.use(express.json());
 
 app.use("/ping", pingRouter);
+
+initDb();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
